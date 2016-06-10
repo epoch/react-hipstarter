1766e8b8c9054860108758bd9667ae0e5ebdb0b1
Daniel Tsui <tsui.daniel@gmail.com>
2016-06-11 01:35:35 +1000

    quick spike demo

--- js/index.js
+++ js/index.js
@@ -1,1 +1,61 @@
-console.log('cakepudding!');
+var React = require('react');
+import { createStore } from 'Redux';
+var ReactDOM = require('react-dom');
+import { Provider, connect } from 'react-redux';
+
+const uid = () => Math.random().toString(34).slice(2);
+
+const addTask = body => ({
+  type: 'ADD_TASK',
+  data: {
+    id: uid(),
+    body: body
+  }
+});
+
+// state transformer for tasks []
+function tasks(tasks = [], action) {
+  switch (action.type) {
+    case 'ADD_TASK': {
+      let newTask = Object.assign({}, action.data);
+      return [...tasks, newTask];
+    }
+    default:
+      return tasks;
+  }
+}
+
+const store = createStore(tasks);
+
+// function that send an action
+window.add = (body) => {
+  store.dispatch(addTask(body));
+}
+
+store.subscribe(function() {
+  console.log(store.getState())
+})
+
+const Todos = React.createClass({
+  render() {
+    return <div>
+      <ul>
+        {this.props.items.map((task) =>
+          <li key={task.id}>{task.body}</li>
+        )}
+      </ul>
+    </div>
+  }
+})
+
+const TodoList = connect(
+  (state) => ({ items: state }),
+  (dispatch) => ({ addTask: body => dispatch(addTask(body)) })
+)(Todos);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoList />
+  </Provider>,
+  document.getElementById('root')
+);
